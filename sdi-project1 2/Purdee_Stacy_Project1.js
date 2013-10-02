ae585109e3033067557d77240535d00dc1c5b2d0
Stacy Purdee <stacy.purdee@gmail.com>
2013-10-01 20:51:02 -0400

    1

--- sdi-project1 2/Purdee_Stacy_Project1.js
+++ sdi-project1 2/Purdee_Stacy_Project1.js
@@ -1,1 +1,14 @@
 //alert("JavaScript works!");
+
+//Stacy A. Purdee
+//SDI 1310
+//Project1
+//Story with JS
+
+var finAid = "$14,250";
+var stuAdvisor = "Advisor \"Galloway\"";
+//var regFunds =
+//var shortFunds =
+var outOfPocket = 360;
+
+console.log("Today my Financial Aid "  + stuAdvisor + " sent me an email saying that I was $" + outOfPocket + " short on this ACYR");
