5a7fc828d2ce91d1b4041e0ab49d3be34ef76506
Stacy Purdee <stacy.purdee@gmail.com>
2013-10-03 13:30:34 -0400

    2

--- sdi-project1 2/Purdee_Stacy_Project1.js
+++ sdi-project1 2/Purdee_Stacy_Project1.js
@@ -7,16 +7,21 @@
 
 //global variables
 
-var finAid = "$14,250";
-var myAdvisor = "Advisor \"Galloway\"";
-var regTuition = "$14,250";
+var strAid = "$14,250";
+var strAdvisor = "Advisor \"Galloway\"";
+var strTuition = "$14,250";
 var shortTuition = "$24,978";
-var outOfPocket = 360;
-var schPayment = 90;
-var tuitionLeft = 5000;
+var intOutOfPocket = 360;
+var intSchPayment = 90;
+var intTuitionLeft = 5000;
+var booMobileStudio = true;
+var mypromt;
+
 
 //my outputs
 
-console.log("Today my Financial Aid "  + myAdvisor + " sent me an email saying that I was $" + outOfPocket + " short on this ACYR.");
-console.log(myAdvisor + " states that I will need too make four payments of $" + schPayment + " each."  );
-console.log("She also stated that I would only have $" + tuitionLeft +" left of Financial Aid money starting the next ACYR.");
+console.log("Today my Financial Aid " + strAdvisor + " sent me an email saying that I was $" + intOutOfPocket + " short on this ACYR.");
+console.log(strAdvisor + " states that I will need too make four payments of $" + intSchPayment + " each.");
+console.log("She also stated that I would only have $" + intTuitionLeft + " left of Financial Aid money starting the next ACYR.");
+
+myprompt = prompt("Isn't it true that I will have a complete mobile studio too learn too program for either Apple or Android?");
